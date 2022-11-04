e74829cebcfa00da8abd120fb34546cd3d9eaec3
Your Name <you@example.com>
2022-11-04 23:54:56 +0600

    first commit

---
+++ lib/models/post_model.dart
@@ -0,0 +1,39 @@
+class Post {
+  String authorName;
+  String authorImageUrl;
+  String timeAgo;
+  String imageUrl;
+
+  Post({
+    required this.authorName,
+    required this.authorImageUrl,
+    required this.timeAgo,
+    required this.imageUrl,
+  });
+}
+
+final List<Post> posts = [
+  Post(
+    authorName: 'Sam Martin',
+    authorImageUrl: 'assets/images/user0.png',
+    timeAgo: '5 min',
+    imageUrl: 'assets/images/post0.jpg',
+  ),
+  Post(
+    authorName: 'Sam Martin',
+    authorImageUrl: 'assets/images/user0.png',
+    timeAgo: '10 min',
+    imageUrl: 'assets/images/post1.jpg',
+  ),
+];
+
+final List<String> stories = [
+  'assets/images/user1.png',
+  'assets/images/user2.png',
+  'assets/images/user3.png',
+  'assets/images/user4.png',
+  'assets/images/user0.png',
+  'assets/images/user1.png',
+  'assets/images/user2.png',
+  'assets/images/user3.png',
+];
